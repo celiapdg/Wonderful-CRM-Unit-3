f1cbf27716b8f5bae8a107ddbf664d2735060a90
Celia <celiapdg@hotmail.com>
2021-01-31 01:21:53 +0100

    Corrected queries, did some testing

--- src/test/java/com/ironhack/CRMunit3/repository/OpportunityRepositoryTest.java
+++ src/test/java/com/ironhack/CRMunit3/repository/OpportunityRepositoryTest.java
@@ -31,19 +31,28 @@ class OpportunityRepositoryTest {
     @BeforeEach
     void setUp() {
         SalesRep salesRep1 = new SalesRep("Pepa Floros");
-        SalesRep salesRep2 = new SalesRep("Pepa Floros");
+        SalesRep salesRep2 = new SalesRep("Husd Hasd");
+
         Contact contact1 = new Contact("Pepa Pig", "676767676", "pepa@pig.pp", "Pigs");
         Contact contact2 = new Contact("Ana Cardo", "656565656", "ana@car.do", "Cards");
+        Contact contact3 = new Contact("Hula Hop", "656565656", "hu@la.hop", "Huli");
+
+        Opportunity opportunity1 = new Opportunity(Product.BOX, 40, contact1, salesRep1);
+        Opportunity opportunity2 = new Opportunity(Product.FLATBED, 23, contact2, salesRep2);
+        Opportunity opportunity3 = new Opportunity(Product.HYBRID, 77, contact3, salesRep2);
+
+        opportunity2.setStatus(Status.CLOSED_LOST);
+        opportunity3.setStatus(Status.CLOSED_WON);
 
         salesRepRepository.saveAll(List.of(salesRep1, salesRep2));
 
         leadRepository.saveAll(List.of(new Lead("Pepa Flores", "666666666", "asda@asd.asd","caca", salesRep1),
-                                       new Lead("Pipo Florín", "666444466", "asda@asd.asd","pedo", salesRep2)));
+                                       new Lead("Pipo Florín", "666444466", "asda@asd.asd","pedo", salesRep2),
+                                       new Lead("Rosa Asor", "626262626", "ros@aa.sor", "pis", salesRep1)));
 
-        contactRepository.saveAll(List.of(contact1,contact2));
+        contactRepository.saveAll(List.of(contact1,contact2, contact3));
 
-        opportunityRepository.saveAll(List.of(new Opportunity(Product.BOX, 40, contact1, salesRep1),
-                                              new Opportunity(Product.FLATBED, 23, contact2, salesRep2)));
+        opportunityRepository.saveAll(List.of(opportunity1, opportunity2, opportunity3));
     }
 
     @AfterEach
@@ -60,13 +69,16 @@ class OpportunityRepositoryTest {
 
     @Test
     void findNumberOfOpportunitiesPerSalesRep() {
+        List <Object[]> result = opportunityRepository.findNumberOfOpportunitiesPerSalesRep();
+        assertEquals(2,result.size());
+        assertEquals(1L,result.get(0)[1]);
+        assertEquals(2L,result.get(1)[1]);
     }
 
     @Test
     void findNumberOfOpportunitiesPerSalesRepWithStatus() {
         List <Object[]> result = opportunityRepository.findNumberOfOpportunitiesPerSalesRepWithStatus(Status.OPEN);
-        assertEquals(2,result.size());
-        assertEquals(1L,result.get(0)[0]);
-        assertEquals(1L,result.get(1)[0]);
+        assertEquals(1,result.size());
+        assertEquals(1L,result.get(0)[1]);
     }
 }
